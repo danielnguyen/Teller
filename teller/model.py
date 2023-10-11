e555ac02cbf0e46bf15d21b7b96df673ff66b79d
Daniel Nguyen <845913+danielnguyen@users.noreply.github.com>
2023-10-11 14:23:34 -0400

    Support pre-2023 BMO statements & add card number

--- teller/model.py
+++ teller/model.py
@@ -6,10 +6,12 @@ class AccountType(Enum):
     MFC = 'MFC'
     RBC = 'RBC'
     BMO = 'BMO'
+    BMO_2022 = 'BMO_2022'
     
 class Transaction():
-    def __init__(self, account_type, date, description, amount):
+    def __init__(self, account_type, card_number, date, description, amount):
         self.account_type = account_type
+        self.card_number = card_number
         self.date = date
         self.description = description
         self.amount = amount
@@ -18,6 +20,7 @@ class Transaction():
         return hash((self.description,
                      self.amount, 
                      self.date, 
+                     self.card_number, 
                      self.account_type))
     
     def __eq__(self, other):
@@ -25,12 +28,12 @@ class Transaction():
                self.account_type == other.account_type and \
                self.date == other.date and \
                self.description == other.description and \
+               self.card_number == other.card_number and \
                self.amount == other.amount
 
     def __repr__(self):
         return (f"({self.amount}, "
                 f"{self.date}, "
                 f"{self.account_type.value}, "
+                f"{self.card_number}, "
                 f"{self.description})")
-
-
