bbc21fea2226e89fcb10689d483dc95eb846642e
Daniel Nguyen <845913+danielnguyen@users.noreply.github.com>
2023-10-21 00:24:42 -0400

    change card to account, delete unneeded things

--- teller.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Absolute path to this script
-SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in
-BASEDIR=$(dirname "$SCRIPT")
-
-# Run Teller
-python $BASEDIR/teller.py "$@"
